b13b5985ebac1bdac644077e31163c8c48daf1a5
Rosana <rosanasrib@gmail.com>
2020-08-04 17:12:51 -0300

    Updates

---
+++ precache-manifest.289ce0c6f8c9f06e34ced142ebe3eaba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c73ee39498277baafa8c84a0c1b496",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "329c9d498b7c17db48f7",
+    "url": "/my-website/static/css/main.ed5d9c2c.chunk.css"
+  },
+  {
+    "revision": "ba8359fb654d93b73ba9",
+    "url": "/my-website/static/js/2.f2154b96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-website/static/js/2.f2154b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329c9d498b7c17db48f7",
+    "url": "/my-website/static/js/main.fa8e1f23.chunk.js"
+  },
+  {
+    "revision": "c8313d6a8cf789ef0797",
+    "url": "/my-website/static/js/runtime-main.4b3f9178.js"
+  },
+  {
+    "revision": "66cd1bd946c0e9ee9d70ad39a0a40b7d",
+    "url": "/my-website/static/media/bg5.66cd1bd9.jpg"
+  },
+  {
+    "revision": "0d049fd3f20c44b012b031541c4bb0c0",
+    "url": "/my-website/static/media/catron.0d049fd3.png"
+  },
+  {
+    "revision": "1712c8bd8f76445b0648c56d07b5ef00",
+    "url": "/my-website/static/media/download.1712c8bd.png"
+  },
+  {
+    "revision": "00798edf2badc6daf052e00fe215ca25",
+    "url": "/my-website/static/media/me.00798edf.jpg"
+  },
+  {
+    "revision": "f86fe38d0362b871addb8e60a040494a",
+    "url": "/my-website/static/media/me.f86fe38d.jpeg"
+  },
+  {
+    "revision": "32b60339274416ea175b134aa3782d3b",
+    "url": "/my-website/static/media/unb.32b60339.png"
+  }
+]);
